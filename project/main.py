156cebadf29e8e855f94e527aacad115b3f93b36
Matheus Julidori <matheus_julidori@yahoo.com.br>
2023-11-30 14:40:31 -0300

    Melhorando apresentação dos dados

--- project/main.py
+++ project/main.py
@@ -1,3 +1,4 @@
+import time
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -24,7 +25,11 @@ train, test = train_test_split(df, test_size=0.2)
 x_train = train.iloc[:, :-1].values
 d_train = train.iloc[:, -1].values
 
+start_time = time.time()
 p.train(x_train, d_train)
+end_time = time.time()
+training_time = (end_time - start_time) * 1000
+training_time = "{:.2f}".format(training_time)
 
 # TESTE
 x_test = test.iloc[:, :-1].values
@@ -32,14 +37,61 @@ d_test = test.iloc[:, -1].values
 
 results = p.test(x_test)
 
-print('Accuracy: ', accuracy_score(d_test, results))
-print('Confusion Matrix: \n', confusion_matrix(d_test, results))
+accuracy = accuracy_score(d_test, results)
+confusion_matrix_results = confusion_matrix(d_test, results)
 print('Classification Report: \n', classification_report(d_test, results))
+print('Tempo de treinamento: ', training_time, 'ms')
 
+# erros por epoca
 plt.plot(np.arange(0, len(p.total_error)), p.total_error)
 plt.title('Erro por época')
 plt.xlabel('Época')
 plt.ylabel('Erro')
+plt.show()
+
+# Gráfico de acuracia
+labels = 'Acertos', 'Erros'
+sizes = [accuracy, 1 - accuracy]
+explode = (0, 0.1)
+title = 'Acurácia do modelo'
+fig1, ax1 = plt.subplots()
+ax1.set_title(title)
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.2f%%', shadow=True, startangle=90)
+ax1.axis('equal')
+plt.show()
+
+# Grafico de matriz de confusão
+true_negatives, false_positives, false_negatives, true_positives = confusion_matrix_results.ravel()
+labels = 'True Negatives', 'False Positives', 'False Negatives', 'True Positives'
+fig2, ax2 = plt.subplots()
+bar_width = 0.35
+indices = np.arange(4)
+bar1 = ax2.bar(indices[0], true_negatives, bar_width, label='True Negatives')
+bar2 = ax2.bar(indices[1], false_positives, bar_width, label='False Positives')
+bar3 = ax2.bar(indices[2], false_negatives, bar_width, label='False Negatives')
+bar4 = ax2.bar(indices[3], true_positives, bar_width, label='True Positives')
+ax2.set_xlabel('Classificação')
+ax2.set_ylabel('Número de ocorrências')
+ax2.set_title('Matriz de confusão')
+ax2.set_xticks(indices)
+ax2.set_xticklabels(['True Negatives', 'False Positives', 'False Negatives', 'True Positives'])
+
+
+def add_values(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax2.annotate('{}'.format(height),
+                     xy=(rect.get_x() + rect.get_width() / 2, height),
+                     xytext=(0, 3),  # 3 points vertical offset
+                     textcoords="offset points",
+                     ha='center', va='bottom')
+
+
+add_values(bar1)
+add_values(bar2)
+add_values(bar3)
+add_values(bar4)
+ax2.legend()
 plt.show()
 
 flag = False
